e43439fa3d46546a8787ac11b133f75779db2a22
Roman Volosatovs <rvolosatovs@riseup.net>
2024-05-30 19:35:39 +0200

    chore: move interfaces to separate repos
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- examples/go/http-outgoing-client/http_outgoing_client.go
+++
@@ -1,3 +0,0 @@
-//go:generate cargo run --bin wit-bindgen-wrpc go --out-dir bindings --package github.com/wrpc/wrpc/examples/go/http-outgoing-client/bindings wit
-
-package http_outgoing_client
--- examples/go/keyvalue-server/keyvalue_server.go
+++
@@ -1,3 +0,0 @@
-//go:generate cargo run --bin wit-bindgen-wrpc go --out-dir bindings --package github.com/wrpc/wrpc/examples/go/keyvalue-server/bindings wit
-
-package keyvalue_server
