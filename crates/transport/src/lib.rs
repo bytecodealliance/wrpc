5cdb96cbfbf219ab6342eb03f471d0b13bf817b6
Roman Volosatovs <rvolosatovs@riseup.net>
2024-06-05 16:27:19 +0200

    doc: document `wrpc_transport`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/transport/src/lib.rs
+++ crates/transport/src/lib.rs
@@ -6,34 +6,63 @@ use bytes::Bytes;
 use futures::Stream;
 use tokio::io::{AsyncRead, AsyncWrite};
 
+/// `Index` implementations are capable of multiplexing underlying connections using a particular
+/// structural `path`
 pub trait Index<T> {
+    /// Multiplexing error
     type Error: Sync + Send;
 
+    /// Index the entity using a structural `path`
     fn index(&self, path: &[usize]) -> Result<T, Self::Error>;
 }
 
+/// Invocation session, which is used to track the lifetime of the data exchange and for
+/// error reporting.
 pub trait Session {
     type Error: FromStr + Display + Sync + Send;
     type TransportError: Sync + Send;
 
+    /// Finish the invocation session, closing all associated communication channels and reporting
+    /// a result to the peer.
     fn finish(
         self,
         res: Result<(), Self::Error>,
     ) -> impl Future<Output = Result<Result<(), Self::Error>, Self::TransportError>> + Send;
 }
 
+/// Invocation encapsulates either server or client-side triple of:
+/// - Multiplexed outgoing byte stream
+/// - Multiplexed incoming byte stream
+/// - Active session, used to communicate and receive transport-layer errors
 pub struct Invocation<O, I, S> {
+    /// Outgoing multiplexed byte stream
     pub outgoing: O,
+
+    /// Incoming multiplexed byte stream
     pub incoming: I,
+
+    /// Invocation session
     pub session: S,
 }
 
+/// Client-side handle to a wRPC transport
 pub trait Invoke: Sync + Send {
+    /// Transport-specific error
     type Error: Sync + Send;
+
+    /// Transport-specific invocation context
     type Context: Sync + Send;
+
+    /// Transport-specific session used for lifetime tracking and error reporting
     type Session: Session + Sync + Send;
+
+    /// Outgoing multiplexed byte stream
     type Outgoing: AsyncWrite + Index<Self::NestedOutgoing> + Sync + Send;
+
+    /// Outgoing multiplexed byte stream, nested at a particular path
     type NestedOutgoing: AsyncWrite + Index<Self::NestedOutgoing> + Sync + Send;
+
+    /// Incoming multiplexed byte stream
     type Incoming: AsyncRead + Index<Self::Incoming> + Sync + Send;
 
     /// Invoke function `func` on instance `instance`
@@ -49,11 +78,21 @@ pub trait Invoke: Sync + Send {
     > + Send;
 }
 
+/// Server-side handle to a wRPC transport
 pub trait Serve {
+    /// Transport-specific error
     type Error: Sync + Send;
+
+    /// Transport-specific invocation context
     type Context: Sync + Send;
+
+    /// Transport-specific session used for lifetime tracking and error reporting
     type Session: Session + Sync + Send;
+
+    /// Outgoing multiplexed byte stream
     type Outgoing: AsyncWrite + Index<Self::Outgoing> + Sync + Send;
+
+    /// Incoming multiplexed byte stream
     type Incoming: AsyncRead + Index<Self::Incoming> + Sync + Send;
 
     /// Serve function `func` from instance `instance`
