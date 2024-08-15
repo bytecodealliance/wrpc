cc03b92503c090e8ca5bd84cf24a65bf4dd28ce8
Roman Volosatovs <rvolosatovs@riseup.net>
2024-08-15 12:33:24 +0200

    doc(rs-wit-bindgen): fix cargo doc build
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/wit-bindgen/src/examples/_1_interface_imports.rs
+++ crates/wit-bindgen/src/examples/_1_interface_imports.rs
@@ -28,6 +28,8 @@ crate::generate!({
     anyhow_path: "anyhow",
     bytes_path: "bytes",
     futures_path: "futures",
+    tokio_util_path: "tokio_util",
+    wasm_tokio_path: "wasm_tokio",
     wrpc_transport_path: "wrpc_transport",
 
     path: "wasi-cli@0.2.0.wasm",
