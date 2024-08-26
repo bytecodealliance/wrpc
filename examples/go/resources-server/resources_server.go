abaef3018ba604d5de6dbe7f5eee5f503ce7f843
Roman Volosatovs <rvolosatovs@riseup.net>
2024-08-26 16:55:52 +0200

    chore: regenerate example bindings
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- examples/go/resources-server/resources_server.go
+++ examples/go/resources-server/resources_server.go
@@ -1,3 +1,3 @@
-//go:generate cargo run --bin wit-bindgen-wrpc go --out-dir bindings --package wrpc.io/examples/go/resources/bindings wit
+//go:generate cargo run --bin wit-bindgen-wrpc go --out-dir bindings --package wrpc.io/examples/go/resources-server/bindings wit
 
 package resources_server
