3c0813ae79413df21ebe26bedfc0e74dd12a3213
Roman Volosatovs <rvolosatovs@riseup.net>
2024-09-25 15:00:14 +0200

    build(nix): update dependencies
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -1,19 +1,17 @@
 {
   nixConfig.extra-substituters = [
-    "https://wrpc.cachix.org"
+    "https://bytecodealliance.cachix.org"
     "https://wasmcloud.cachix.org"
     "https://nixify.cachix.org"
     "https://crane.cachix.org"
-    "https://bytecodealliance.cachix.org"
     "https://nix-community.cachix.org"
     "https://cache.garnix.io"
   ];
   nixConfig.extra-trusted-public-keys = [
-    "wrpc.cachix.org-1:J1xnzWo1nnhlzOmZCA10/5wz87LwCFwQtnqCibCy67w="
+    "bytecodealliance.cachix.org-1:0SBgh//n2n0heh0sDFhTm+ZKBRy2sInakzFGfzN531Y="
     "wasmcloud.cachix.org-1:9gRBzsKh+x2HbVVspreFg/6iFRiD4aOcUQfXVDl3hiM="
     "nixify.cachix.org-1:95SiUQuf8Ij0hwDweALJsLtnMyv/otZamWNRp1Q1pXw="
     "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
-    "bytecodealliance.cachix.org-1:0SBgh//n2n0heh0sDFhTm+ZKBRy2sInakzFGfzN531Y="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
@@ -228,7 +226,7 @@
               pkgs.natscli
               pkgs.wit-deps
 
-              pkgs.pkgsUnstable.go
+              pkgs.pkgsUnstable.go_1_23
             ];
           }
           devShells;
