3647c0efa40a3619aeb4083530247dee3113b837
Roman Volosatovs <rvolosatovs@riseup.net>
2024-10-25 15:57:09 +0200

    bench(nats): add `greet` benchmark
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,29 +6,54 @@ use futures::StreamExt as _;
 use tokio::select;
 use tokio::sync::oneshot;
 
-mod rtt_bindings {
+mod ping_bindings {
     wit_bindgen_wrpc::generate!({
         inline: "
-            package wrpc-bench:rtt;
+            package wrpc-bench:ping;
 
-            interface rtt {
+            interface ping {
                 ping: func();
             }
 
             world proxy {
-                import rtt;
-                export rtt;
+                import ping;
+                export ping;
+            }
+"
+    });
+}
+mod greet_bindings {
+    wit_bindgen_wrpc::generate!({
+        inline: "
+            package wrpc-bench:greet;
+
+            interface greet {
+                greet: func(name: string) -> string;
+            }
+
+            world proxy {
+                import greet;
+                export greet;
             }
 "
     });
 }
 
+use greet_bindings::exports::wrpc_bench::greet::greet::Handler as _;
+use ping_bindings::exports::wrpc_bench::ping::ping::Handler as _;
+
 #[derive(Clone, Copy)]
 struct RttHandler;
 
-impl<T: Send> rtt_bindings::exports::wrpc_bench::rtt::rtt::Handler<T> for RttHandler {
+impl<T: Send> ping_bindings::exports::wrpc_bench::ping::ping::Handler<T> for RttHandler {
     async fn ping(&self, _cx: T) -> anyhow::Result<()> {
         Ok(())
+    }
+}
+
+impl<T: Send> greet_bindings::exports::wrpc_bench::greet::greet::Handler<T> for RttHandler {
+    async fn greet(&self, _cx: T, name: String) -> anyhow::Result<String> {
+        Ok(format!("Hello, {name}"))
     }
 }
 
@@ -46,12 +71,12 @@ pub fn with_nats<T>(
     Ok(res)
 }
 
-fn bench_nats_base_rtt(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Result<()> {
+fn bench_nats_base_ping(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Result<()> {
     with_nats(|rt, nats_clt| {
         const SUBJECT: &str = "wrpc.0.0.1.wrpc-bench:rtt/rtt.ping";
         let mut sub = rt
             .block_on(nats_clt.subscribe(SUBJECT))
-            .context("failed to subscribe on echo subject")?;
+            .context("failed to subscribe on subject")?;
 
         let (stop_tx, mut stop_rx) = oneshot::channel();
         let srv = rt.spawn({
@@ -63,13 +88,16 @@ fn bench_nats_base_rtt(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Resu
                             payload,
                             status,
                             reply,
+                            headers,
                             ..
                         }) = sub.next() => {
                             assert!(status.is_none());
+                            assert!(payload.is_empty());
                             let reply = reply.context("reply subject missing")?;
-                            nats_clt.publish(reply, payload)
+                            RttHandler.ping(headers).await.context("failed to call handler")?;
+                            nats_clt.publish(reply, Bytes::default())
                                 .await
-                                .context("failed to publish message")?;
+                                .context("failed to publish response")?;
                         }
                         _ = &mut stop_rx => {
                             sub.unsubscribe().await.context("failed to unsubscribe")?;
@@ -97,12 +125,109 @@ fn bench_nats_base_rtt(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Resu
     })
 }
 
-fn bench_nats_wrpc_rtt(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Result<()> {
+fn bench_nats_base_greet(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Result<()> {
+    with_nats(|rt, nats_clt| {
+        const SUBJECT: &str = "wrpc.0.0.1.wrpc-bench:rtt/rtt.greet";
+        let mut sub = rt
+            .block_on(nats_clt.subscribe(SUBJECT))
+            .context("failed to subscribe on subject")?;
+
+        let (stop_tx, mut stop_rx) = oneshot::channel();
+        let srv = rt.spawn({
+            let nats_clt = nats_clt.clone();
+            async move {
+                loop {
+                    select! {
+                        Some(async_nats::Message {
+                            payload,
+                            status,
+                            reply,
+                            headers,
+                            ..
+                        }) = sub.next() => {
+                            assert!(status.is_none());
+                            let reply = reply.context("reply subject missing")?;
+                            let payload = String::from_utf8(Vec::from(payload)).context("payload in not UTF-8")?;
+                            let payload = RttHandler.greet(headers, payload).await.context("failed to call handler")?;
+                            nats_clt.publish(reply, Bytes::from(payload))
+                                .await
+                                .context("failed to publish response")?;
+                        }
+                        _ = &mut stop_rx => {
+                            sub.unsubscribe().await.context("failed to unsubscribe")?;
+                            return anyhow::Ok(())
+                        }
+                    }
+                }
+            }
+        });
+        g.bench_function("direct", |b| {
+            b.to_async(rt).iter(|| async {
+                let async_nats::Message {
+                    payload, status, ..
+                } = nats_clt
+                    .request(SUBJECT, Bytes::from("test"))
+                    .await
+                    .expect("failed to send message");
+                assert!(status.is_none());
+                assert_eq!(payload, b"Hello, test".as_slice());
+            })
+        });
+        stop_tx.send(()).expect("failed to stop `echo`");
+        rt.block_on(async { srv.await.context("`echo` task panicked")? })?;
+        Ok(())
+    })
+}
+
+fn bench_nats_wrpc_ping(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Result<()> {
     with_nats(|rt, nats_clt| {
         let wrpc = wrpc_transport_nats::Client::new(nats_clt, "", None);
 
         let invocations = rt
-            .block_on(rtt_bindings::serve(&wrpc, RttHandler))
+            .block_on(ping_bindings::serve(&wrpc, RttHandler))
+            .context("failed to serve bindings")?;
+        let mut invocations = invocations.into_iter();
+        let (Some((_, _, mut invocations)), None) = (invocations.next(), invocations.next()) else {
+            bail!("invalid invocation stream vector")
+        };
+        let (stop_tx, mut stop_rx) = oneshot::channel();
+        let handle = rt.handle().clone();
+        let srv = handle.spawn({
+            let handle = handle.clone();
+            async move {
+                loop {
+                    select! {
+                        Some(res) = invocations.next() => {
+                            let fut = res.expect("failed to accept invocation");
+                            handle.spawn(fut);
+                        },
+                        _ = &mut stop_rx => {
+                            drop(invocations);
+                            return anyhow::Ok(())
+                        },
+                    }
+                }
+            }
+        });
+        g.bench_function("wRPC", |b| {
+            b.to_async(rt).iter(|| async {
+                ping_bindings::wrpc_bench::ping::ping::ping(&wrpc, None)
+                    .await
+                    .expect("failed to call `ping`");
+            })
+        });
+        stop_tx.send(()).expect("failed to stop server");
+        rt.block_on(async { srv.await.context("server task panicked")? })?;
+        Ok(())
+    })
+}
+
+fn bench_nats_wrpc_greet(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Result<()> {
+    with_nats(|rt, nats_clt| {
+        let wrpc = wrpc_transport_nats::Client::new(nats_clt, "", None);
+
+        let invocations = rt
+            .block_on(greet_bindings::serve(&wrpc, RttHandler))
             .context("failed to serve bindings")?;
         let mut invocations = invocations.into_iter();
         let (Some((_, _, mut invocations)), None) = (invocations.next(), invocations.next()) else {
@@ -128,9 +253,10 @@ fn bench_nats_wrpc_rtt(g: &mut BenchmarkGroup<impl Measurement>) -> anyhow::Resu
         });
         g.bench_function("wRPC", |b| {
             b.to_async(rt).iter(|| async {
-                rtt_bindings::wrpc_bench::rtt::rtt::ping(&wrpc, None)
+                let greeting = greet_bindings::wrpc_bench::greet::greet::greet(&wrpc, None, "test")
                     .await
-                    .expect("failed to call `ping`");
+                    .expect("failed to call `greet`");
+                assert_eq!(greeting, "Hello, test");
             })
         });
         stop_tx.send(()).expect("failed to stop server");
@@ -143,8 +269,14 @@ fn main() -> anyhow::Result<()> {
     let mut c = Criterion::default().configure_from_args();
     {
         let mut g = c.benchmark_group("NATS.io ping RTT");
-        bench_nats_base_rtt(&mut g)?;
-        bench_nats_wrpc_rtt(&mut g)?;
+        bench_nats_base_ping(&mut g)?;
+        bench_nats_wrpc_ping(&mut g)?;
+        g.finish();
+    }
+    {
+        let mut g = c.benchmark_group("NATS.io greet RTT");
+        bench_nats_base_greet(&mut g)?;
+        bench_nats_wrpc_greet(&mut g)?;
         g.finish();
     }
     c.final_summary();
