59bc861a389f4b60eb305a84ad2df138428caf0b
Roman Volosatovs <rvolosatovs@riseup.net>
2024-11-05 20:14:10 +0100

    feat(web-keyvalue): wire up wRPC invocations to buttons
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- examples/web/rust/src/main.rs
+++ examples/web/rust/src/main.rs
@@ -2,6 +2,7 @@ use core::net::SocketAddr;
 use core::pin::pin;
 use core::time::Duration;
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use anyhow::Context as _;
@@ -9,26 +10,159 @@ use axum::http::header::CONTENT_TYPE;
 use axum::response::Html;
 use axum::routing::get;
 use axum::Router;
+use bytes::Bytes;
 use clap::Parser;
 use futures::stream::select_all;
 use futures::StreamExt as _;
+use tokio::sync::RwLock;
 use tokio::task::JoinSet;
 use tokio::{select, signal};
 use tower_http::trace::TraceLayer;
-use tracing::{debug, error, info, trace, warn};
+use tracing::{debug, error, info, instrument, trace, warn};
 use tracing_subscriber::layer::SubscriberExt as _;
 use tracing_subscriber::util::SubscriberInitExt as _;
+use url::Url;
+use uuid::Uuid;
+use wrpc_transport::{ResourceBorrow, ResourceOwn};
+use wrpc_wasi_keyvalue::exports::wasi::keyvalue::store;
 use wtransport::tls::Sha256DigestFmt;
 use wtransport::{Endpoint, Identity, ServerConfig};
 
-//#[derive(Clone, Copy)]
-//struct Handler;
-//
-//impl bindings::exports::wrpc_examples::hello::handler::Handler<()> for Handler {
-//    async fn hello(&self, (): ()) -> anyhow::Result<String> {
-//        Ok("hello from Rust".to_string())
-//    }
-//}
+pub type Result<T, E = store::Error> = core::result::Result<T, E>;
+
+#[derive(Clone, Debug, Default)]
+struct Handler {
+    buckets: Arc<RwLock<HashMap<Bytes, Bucket>>>,
+    mem: wrpc_wasi_keyvalue_mem::Handler,
+}
+
+impl Handler {
+    async fn bucket(&self, bucket: impl AsRef<[u8]>) -> Result<Bucket> {
+        trace!("looking up bucket");
+        let store = self.buckets.read().await;
+        store
+            .get(bucket.as_ref())
+            .ok_or(store::Error::NoSuchStore)
+            .cloned()
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Bucket {
+    Mem(ResourceOwn<store::Bucket>),
+}
+
+impl<C: Send + Sync> store::Handler<C> for Handler {
+    // NOTE: Resource handle returned is just the `identifier` itself
+    #[instrument(level = "trace", skip(cx), ret(level = "trace"))]
+    async fn open(
+        &self,
+        cx: C,
+        identifier: String,
+    ) -> anyhow::Result<Result<ResourceOwn<store::Bucket>>> {
+        if identifier.is_empty() {
+            let bucket = match self.mem.open(cx, identifier).await? {
+                Ok(bucket) => bucket,
+                Err(err) => return Ok(Err(err)),
+            };
+            let id = Uuid::now_v7();
+            let id = Bytes::copy_from_slice(id.as_bytes());
+            let mut buckets = self.buckets.write().await;
+            buckets.insert(id.clone(), Bucket::Mem(bucket));
+            return Ok(Ok(ResourceOwn::from(id)));
+        }
+        let url = match Url::parse(&identifier) {
+            Ok(url) => url,
+            Err(err) => return Ok(Err(store::Error::Other(err.to_string()))),
+        };
+        match url.scheme() {
+            "wrpc+nats" => {
+                eprintln!("TODO: NATS");
+            }
+            "wrpc+quic" => {
+                eprintln!("TODO: QUIC");
+            }
+            "wrpc+tcp" => {
+                eprintln!("TODO: TCP");
+            }
+            "wrpc+uds" => {
+                eprintln!("TODO: UDS");
+            }
+            "wrpc+web" => {
+                eprintln!("WebTransport");
+            }
+            scheme => {
+                return Ok(Err(store::Error::Other(format!(
+                    "unsupported scheme: {scheme}"
+                ))))
+            }
+        }
+        return Ok(Err(store::Error::Other("unsupported".into())));
+    }
+}
+
+impl<C: Send + Sync> store::HandlerBucket<C> for Handler {
+    #[instrument(level = "trace", skip(cx), ret(level = "trace"))]
+    async fn get(
+        &self,
+        cx: C,
+        bucket: ResourceBorrow<store::Bucket>,
+        key: String,
+    ) -> anyhow::Result<Result<Option<Bytes>>> {
+        match self.bucket(bucket).await? {
+            Bucket::Mem(bucket) => self.mem.get(cx, bucket.as_borrow(), key).await,
+        }
+    }
+
+    #[instrument(level = "trace", skip(cx), ret(level = "trace"))]
+    async fn set(
+        &self,
+        cx: C,
+        bucket: ResourceBorrow<store::Bucket>,
+        key: String,
+        value: Bytes,
+    ) -> anyhow::Result<Result<()>> {
+        match self.bucket(bucket).await? {
+            Bucket::Mem(bucket) => self.mem.set(cx, bucket.as_borrow(), key, value).await,
+        }
+    }
+
+    #[instrument(level = "trace", skip(cx), ret(level = "trace"))]
+    async fn delete(
+        &self,
+        cx: C,
+        bucket: ResourceBorrow<store::Bucket>,
+        key: String,
+    ) -> anyhow::Result<Result<()>> {
+        match self.bucket(bucket).await? {
+            Bucket::Mem(bucket) => self.mem.delete(cx, bucket.as_borrow(), key).await,
+        }
+    }
+
+    #[instrument(level = "trace", skip(cx), ret(level = "trace"))]
+    async fn exists(
+        &self,
+        cx: C,
+        bucket: ResourceBorrow<store::Bucket>,
+        key: String,
+    ) -> anyhow::Result<Result<bool>> {
+        match self.bucket(bucket).await? {
+            Bucket::Mem(bucket) => self.mem.exists(cx, bucket.as_borrow(), key).await,
+        }
+    }
+
+    #[instrument(level = "trace", skip(cx), ret(level = "trace"))]
+    async fn list_keys(
+        &self,
+        cx: C,
+        bucket: ResourceBorrow<store::Bucket>,
+        cursor: Option<String>,
+    ) -> anyhow::Result<Result<store::KeyResponse>> {
+        match self.bucket(bucket).await? {
+            Bucket::Mem(bucket) => self.mem.list_keys(cx, bucket.as_borrow(), cursor).await,
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -38,17 +172,14 @@ struct Args {
     addr: SocketAddr,
 }
 
-#[must_use]
-pub fn env_filter() -> tracing_subscriber::EnvFilter {
-    tracing_subscriber::EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info"))
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer().compact().without_time())
-        .with(env_filter())
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
         .init();
 
     let Args { addr } = Args::parse();
@@ -140,7 +271,7 @@ export const PORT = "{port}"
 
     let invocations = wrpc_wasi_keyvalue::exports::wasi::keyvalue::store::serve_interface(
         srv.as_ref(),
-        wrpc_wasi_keyvalue_mem::Handler::default(),
+        Handler::default(),
     )
     .await
     .context("failed to serve `wasi:keyvalue`")?;
