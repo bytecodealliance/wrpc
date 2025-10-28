ec90f2955e83a9c86c3977ac9c5e14d54b1e1916
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-10-28 16:38:38 -0700

    Implement hello-quic-client and hello-quic-server

---
+++ examples/rust/hello-quic-client/src/main.rs
@@ -0,0 +1,96 @@
+use std::sync::Arc;
+
+use anyhow::Context as _;
+use clap::Parser;
+use core::net::SocketAddr;
+use quinn::Endpoint;
+use quinn::{crypto::rustls::QuicClientConfig, ClientConfig};
+use rustls::{
+    client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier},
+    pki_types::{CertificateDer, ServerName, UnixTime},
+    version::TLS13,
+    DigitallySignedStruct, SignatureScheme,
+};
+
+mod bindings {
+    wit_bindgen_wrpc::generate!({
+        with: {
+            "wrpc-examples:hello/handler": generate
+        }
+    });
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Address to invoke `wrpc-examples:hello/handler.hello` on
+    #[arg(default_value = "127.0.0.1:4433")]
+    addr: SocketAddr,
+}
+
+#[derive(Debug)]
+struct Insecure;
+
+impl ServerCertVerifier for Insecure {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _server_name: &ServerName<'_>,
+        _ocsp_response: &[u8],
+        _now: UnixTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        vec![SignatureScheme::ECDSA_NISTP256_SHA256]
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    let Args { addr } = Args::parse();
+
+    let conf = rustls::ClientConfig::builder_with_protocol_versions(&[&TLS13])
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(Insecure))
+        .with_no_client_auth();
+
+    let conf: ClientConfig = ClientConfig::new(Arc::new(QuicClientConfig::try_from(conf)?));
+    // Bind to any IPv4 or IPv6 address (dual stack, if supported).
+    let ep = Endpoint::client((std::net::Ipv6Addr::UNSPECIFIED, 0).into())?;
+
+    // Connect using the rustls client configuration, addr, and server name
+    let connection = ep
+        .connect_with(conf, addr, "localhost")?
+        .await
+        .context("failed to connect to server")?;
+    let wrpc = wrpc_transport_quic::Client::from(connection);
+    let hello = bindings::wrpc_examples::hello::handler::hello(&wrpc, ())
+        .await
+        .context("failed to invoke `wrpc-examples.hello/handler.hello`")?;
+    eprintln!("{hello}");
+
+    Ok(())
+}
---
+++ examples/rust/hello-quic-server/src/main.rs
@@ -0,0 +1,163 @@
+use core::net::SocketAddr;
+use core::pin::pin;
+
+use std::sync::Arc;
+
+use anyhow::Context as _;
+use clap::Parser;
+use futures::stream::select_all;
+use futures::StreamExt as _;
+use quinn::crypto::rustls::QuicServerConfig;
+use quinn::{Endpoint, ServerConfig};
+use rcgen::{generate_simple_self_signed, CertifiedKey};
+use rustls::pki_types::{CertificateDer, PrivatePkcs8KeyDer};
+use rustls::version::TLS13;
+use tokio::task::JoinSet;
+use tokio::{select, signal};
+use tracing::{debug, error, info, warn};
+
+mod bindings {
+    wit_bindgen_wrpc::generate!({
+        with: {
+            "wrpc-examples:hello/handler": generate,
+        }
+    });
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Address to serve `wasi:keyvalue` on
+    #[arg(default_value = "[::1]:4433")]
+    addr: SocketAddr,
+}
+
+#[derive(Clone, Copy)]
+struct Handler;
+
+impl bindings::exports::wrpc_examples::hello::handler::Handler<()> for Handler {
+    async fn hello(&self, (): ()) -> anyhow::Result<String> {
+        Ok("hello from Rust".to_string())
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    let Args { addr } = Args::parse();
+
+    let CertifiedKey { cert, key_pair } = generate_simple_self_signed([
+        "localhost".to_string(),
+        "::1".to_string(),
+        "127.0.0.1".to_string(),
+    ])
+    .context("failed to generate server certificate")?;
+    let cert = CertificateDer::from(cert);
+
+    let conf = rustls::ServerConfig::builder_with_protocol_versions(&[&TLS13])
+        .with_no_client_auth() // TODO: verify client cert
+        .with_single_cert(
+            vec![cert],
+            PrivatePkcs8KeyDer::from(key_pair.serialize_der()).into(),
+        )
+        .context("failed to create server config")?;
+
+    let conf: QuicServerConfig = conf
+        .try_into()
+        .context("failed to convert rustls client config to QUIC server config")?;
+
+    let ep = Endpoint::server(ServerConfig::with_crypto(Arc::new(conf)), addr)
+        .context("failed to create server endpoint")?;
+
+    let srv = Arc::new(wrpc_transport_quic::Server::new());
+    let accept = tokio::spawn({
+        let mut tasks = JoinSet::<anyhow::Result<()>>::new();
+        let srv = Arc::clone(&srv);
+        async move {
+            loop {
+                select! {
+                    Some(conn) = ep.accept() => {
+                        let srv = Arc::clone(&srv);
+                        tasks.spawn(async move {
+                            let conn = conn
+                                .accept()
+                                .context("failed to accept QUIC connection")?;
+                            let conn = conn.await.context("failed to establish QUIC connection")?;
+                            let wrpc = wrpc_transport_quic::Client::from(conn);
+                            loop {
+                                srv.accept(&wrpc)
+                                    .await
+                                    .context("failed to accept wRPC connection")?;
+                            }
+                        });
+                    }
+                    Some(res) = tasks.join_next() => {
+                        match res {
+                            Ok(Ok(())) => {}
+                            Ok(Err(err)) => {
+                                warn!(?err, "failed to serve connection")
+                            }
+                            Err(err) => {
+                                error!(?err, "failed to join task")
+                            }
+                        }
+                    }
+                    else => {
+                        return;
+                    }
+                }
+            }
+        }
+    });
+
+    let invocations = bindings::serve(srv.as_ref(), Handler)
+        .await
+        .context("failed to serve `wasi:keyvalue`")?;
+    // NOTE: This will conflate all invocation streams into a single stream via `futures::stream::SelectAll`,
+    // to customize this, iterate over the returned `invocations` and set up custom handling per export
+    let mut invocations = select_all(
+        invocations
+            .into_iter()
+            .map(|(instance, name, invocations)| invocations.map(move |res| (instance, name, res))),
+    );
+    let shutdown = signal::ctrl_c();
+    let mut shutdown = pin!(shutdown);
+    let mut tasks = JoinSet::new();
+    loop {
+        select! {
+            Some((instance, name, res)) = invocations.next() => {
+                match res {
+                    Ok(fut) => {
+                        debug!(instance, name, "invocation accepted");
+                        tasks.spawn(async move {
+                            if let Err(err) = fut.await {
+                                warn!(?err, "failed to handle invocation");
+                            } else {
+                                info!(instance, name, "invocation successfully handled");
+                            }
+                        });
+                    }
+                    Err(err) => {
+                        warn!(?err, instance, name, "failed to accept invocation");
+                    }
+                }
+            }
+            Some(res) = tasks.join_next() => {
+                if let Err(err) = res {
+                    error!(?err, "failed to join task")
+                }
+            }
+            res = &mut shutdown => {
+                accept.abort();
+                // wait for all invocations to complete
+                while let Some(res) = tasks.join_next().await {
+                    if let Err(err) = res {
+                        error!(?err, "failed to join task")
+                    }
+                }
+                return res.context("failed to listen for ^C")
+            }
+        }
+    }
+}
