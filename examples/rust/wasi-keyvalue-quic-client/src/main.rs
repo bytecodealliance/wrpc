521ca77cb1a4e3bf32534fd7c7006e42de2372fc
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-10-27 18:29:51 -0700

    Implement wasi-keyvalue-quic-client

--- examples/rust/wasi-keyvalue-quic-client/src/main.rs
+++ examples/rust/wasi-keyvalue-quic-client/src/main.rs
@@ -1,30 +1,85 @@
-mod client_config;
-
-use client_config::configure_client;
+use std::sync::Arc;
 
 use anyhow::{ensure, Context as _};
 use bytes::Bytes;
+use clap::Parser;
 use core::net::SocketAddr;
 use quinn::Endpoint;
-use std::net::{IpAddr, Ipv4Addr};
+use quinn::{crypto::rustls::QuicClientConfig, ClientConfig};
+use rustls::{
+    client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier},
+    pki_types::{CertificateDer, ServerName, UnixTime},
+    version::TLS13,
+    DigitallySignedStruct, SignatureScheme,
+};
 use wrpc_wasi_keyvalue::wasi::keyvalue::store;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Address to invoke `wrpc-examples:hello/handler.hello` on
+    #[arg(default_value = "127.0.0.1:4433")]
+    addr: SocketAddr,
+}
+
+#[derive(Debug)]
+struct Insecure;
+
+impl ServerCertVerifier for Insecure {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _server_name: &ServerName<'_>,
+        _ocsp_response: &[u8],
+        _now: UnixTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        vec![SignatureScheme::ECDSA_NISTP256_SHA256]
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().init();
 
-    let client_config = configure_client().context("failed tls verification")?;
-    const SERVER_NAME: &str = "localhost";
-    const LOCALHOST_V4: IpAddr = IpAddr::V4(Ipv4Addr::LOCALHOST);
-    const CLIENT_ADDR: SocketAddr = SocketAddr::new(LOCALHOST_V4, 5000);
-    const SERVER_ADDR: SocketAddr = SocketAddr::new(LOCALHOST_V4, 4433);
-    // Bind this endpoint to a UDP socket on the given client address.
-    let endpoint = Endpoint::client(CLIENT_ADDR)?;
+    let Args { addr } = Args::parse();
 
-    // Connect to the server passing in the server name which is supposed to be in the server certificate.
-    let connection = endpoint
-        .connect_with(client_config, SERVER_ADDR, SERVER_NAME)?
-        .await?;
+    let conf = rustls::ClientConfig::builder_with_protocol_versions(&[&TLS13])
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(Insecure))
+        .with_no_client_auth();
+
+    let conf: ClientConfig = ClientConfig::new(Arc::new(QuicClientConfig::try_from(conf)?));
+    // Bind to any IPv4 or IPv6 address (dual stack, if supported).
+    let ep = Endpoint::client((std::net::Ipv6Addr::UNSPECIFIED, 0).into())?;
+
+    // Connect using the rustls client configuration, addr, and server name
+    let connection = ep
+        .connect_with(conf, addr, "localhost")?
+        .await
+        .context("failed to connect to server")?;
     let wrpc = wrpc_transport_quic::Client::from(connection);
     let bucket = store::open(&wrpc, (), "example")
         .await
