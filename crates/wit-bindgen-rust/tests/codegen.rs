2c6c8b743e29a9993778b5c65339775cffe6b826
Roman Volosatovs <rvolosatovs@riseup.net>
2026-06-23 18:56:41 +0200

    chore: copy wit-bindgen 0.58 rust codegen test
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/wit-bindgen-rust/tests/codegen.rs
+++ crates/wit-bindgen-rust/tests/codegen.rs
@@ -37,6 +37,156 @@ mod inline_and_path {
 }
 
 #[allow(unused, reason = "testing codegen, not functionality")]
+mod newtyped_list {
+    use std::ops::Deref;
+
+    wit_bindgen_wrpc::generate!({
+        inline: r#"
+        package test:newtyped-list;
+
+        interface byte {
+            type typed-list-of-byte = list<u8>;
+            type newtyped-list-of-byte = list<u8>;
+
+            record rec-of-lists {
+                l: list<u8>,
+                tl: typed-list-of-byte,
+                nl: newtyped-list-of-byte,
+            }
+
+            use-list-of-byte: func(l: list<u8>) -> list<u8>;
+            use-typed-list-of-byte: func(tl: typed-list-of-byte) -> typed-list-of-byte;
+            use-newtyped-list-of-byte: func(nl: newtyped-list-of-byte) -> newtyped-list-of-byte;
+            use-rec-of-lists: func(t: rec-of-lists) -> rec-of-lists;
+        }
+
+        interface noncopy-byte {
+            // this will be new-typed by a non-copy struct
+            type noncopy-byte = u8;
+
+            type newtyped-list-of-noncopy-byte = list<noncopy-byte>;
+            type typed-list-of-noncopy-byte = list<noncopy-byte>;
+
+            record rec-of-lists-of-noncopy-byte {
+                ntl: newtyped-list-of-noncopy-byte,
+                tl: typed-list-of-noncopy-byte,
+                l: list<noncopy-byte>,
+            }
+
+            use-newtyped-list-of-noncopy-byte: func(nl: newtyped-list-of-noncopy-byte) -> newtyped-list-of-noncopy-byte;
+            use-typed-list-of-noncopy-byte: func(tl: typed-list-of-noncopy-byte) -> typed-list-of-noncopy-byte;
+            use-list-of-noncopy-byte: func(l: list<noncopy-byte>) -> list<noncopy-byte>;
+            use-rec-of-lists-of-noncopy-byte: func(t: rec-of-lists-of-noncopy-byte) -> rec-of-lists-of-noncopy-byte;
+        }
+
+        world test {
+            import byte;
+            export byte;
+            import noncopy-byte;
+            export noncopy-byte;
+        }
+        "#,
+        with: {
+            "test:newtyped-list/byte/newtyped-list-of-byte": crate::newtyped_list::NewtypedListOfByte,
+            "test:newtyped-list/noncopy-byte/noncopy-byte": crate::newtyped_list::NoncopyByte,
+            "test:newtyped-list/noncopy-byte/newtyped-list-of-noncopy-byte": crate::newtyped_list::NewtypedListofNoncopyByte,
+        }
+    });
+
+    #[derive(Debug, Clone)]
+    pub struct NewtypedListOfByte(Vec<u8>);
+
+    impl From<Vec<u8>> for NewtypedListOfByte {
+        fn from(value: Vec<u8>) -> Self {
+            NewtypedListOfByte(value)
+        }
+    }
+
+    impl From<NewtypedListOfByte> for Vec<u8> {
+        fn from(value: NewtypedListOfByte) -> Self {
+            value.0
+        }
+    }
+
+    impl Deref for NewtypedListOfByte {
+        type Target = Vec<u8>;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct NoncopyByte(u8);
+
+    #[derive(Debug, Clone)]
+    pub struct NewtypedListofNoncopyByte(Vec<NoncopyByte>);
+
+    impl From<Vec<NoncopyByte>> for NewtypedListofNoncopyByte {
+        fn from(value: Vec<NoncopyByte>) -> Self {
+            NewtypedListofNoncopyByte(value)
+        }
+    }
+
+    impl From<NewtypedListofNoncopyByte> for Vec<NoncopyByte> {
+        fn from(value: NewtypedListofNoncopyByte) -> Self {
+            value.0
+        }
+    }
+
+    impl Deref for NewtypedListofNoncopyByte {
+        type Target = Vec<NoncopyByte>;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+}
+
+#[allow(unused, reason = "testing codegen, not functionality")]
+mod map_type_hashmap {
+    wit_bindgen_wrpc::generate!({
+        inline: r#"
+        package test:map-type;
+
+        interface maps {
+            type names-by-id = map<u32, string>;
+
+            roundtrip: func(a: names-by-id) -> names-by-id;
+            inline-roundtrip: func(a: map<string, u32>) -> map<string, u32>;
+        }
+
+        world test {
+            import maps;
+            export maps;
+        }
+        "#,
+        map_type: "std::collections::HashMap",
+        generate_all,
+    });
+}
+
+#[allow(unused, reason = "testing codegen, not functionality")]
+mod map_type_default {
+    wit_bindgen_wrpc::generate!({
+        inline: r#"
+        package test:map-default;
+
+        interface maps {
+            type names-by-id = map<u32, string>;
+            nested: func(a: map<string, map<u32, string>>) -> map<string, map<u32, string>>;
+        }
+
+        world test {
+            import maps;
+            export maps;
+        }
+        "#,
+        generate_all,
+    });
+}
+
+#[allow(unused, reason = "testing codegen, not functionality")]
 mod retyped_list {
     use std::ops::Deref;
 
@@ -64,5 +214,24 @@ mod retyped_list {
         with: {
             "test:retyped-list/bytes/retyped-list-as-bytes": bytes::Bytes,
         }
+    });
+}
+
+#[allow(unused, reason = "testing codegen, not functionality")]
+mod method_chaining {
+    wit_bindgen_wrpc::generate!({
+        inline: r#"
+        package test:method-chaining;
+        world test {
+            resource a {
+                constructor();
+                set-a: func(arg: u32);
+                set-b: func(arg: bool);
+                do: func();
+            }
+        }
+        "#,
+        generate_all,
+        enable_method_chaining: true
     });
 }
